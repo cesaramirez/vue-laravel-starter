1c2f1cb993e9badc6c990aee52c1f9afc865c947
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-12-12 19:48:37 -0600

    :sparkles: Done logic and form Login

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import router from "@/router";
+import store from "@/store";
 import App from "@/components/App";
 import Vuetify from "vuetify";
 
@@ -10,5 +11,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   ...App
 });
--- resources/assets/js/pages/auth/login.vue
+++ resources/assets/js/pages/auth/login.vue
@@ -6,41 +6,42 @@
                     <div class="headline">Iniciar Sesión</div>
                     <small>Porfavor ingresar los datos del formulario siguiente para que puedas iniciar sesión en la aplicación.</small>
                 </v-card-title>
-                <v-card-text>
-                    <v-layout row>
-                        <v-flex xs12>
-                            <v-text-field
-                            v-model="form.email"
-                            name="email"
-                            label="Ingrese su Correo Electrónico"
-                            dark
-                            prepend-icon="mail"
-                            ></v-text-field>
-                        </v-flex>
-                    </v-layout>
-                    <v-layout row>
-                        <v-flex xs12>
-                            <v-text-field
-                            v-model="form.password"
-                            name="password"
-                            label="Ingrese su Contraseña"
-                            type="password"
-                            dark
-                            prepend-icon="vpn_key"
-                            ></v-text-field>
-                        </v-flex>
-                    </v-layout>
-                </v-card-text>
-                <v-card-actions>
-                    <v-btn flat dark @click="login">Ingresar</v-btn>
-                </v-card-actions>
+                <v-form @submit.prevent="login">
+                  <v-card-text>
+                      <v-layout row>
+                          <v-flex xs12>
+                              <v-text-field
+                              v-model="form.email"
+                              name="email"
+                              label="Ingrese su Correo Electrónico"
+                              dark
+                              prepend-icon="mail"
+                              ></v-text-field>
+                          </v-flex>
+                      </v-layout>
+                      <v-layout row>
+                          <v-flex xs12>
+                              <v-text-field
+                              v-model="form.password"
+                              name="password"
+                              label="Ingrese su Contraseña"
+                              type="password"
+                              dark
+                              prepend-icon="vpn_key"
+                              ></v-text-field>
+                          </v-flex>
+                      </v-layout>
+                  </v-card-text>
+                  <v-card-actions>
+                      <v-btn flat dark type="submit">Ingresar</v-btn>
+                  </v-card-actions>
+                </v-form>
             </v-card>
         </v-flex>
     </v-layout>
 </template>
 
 <script>
-
 export default {
   layout: 'auth',
   data: () => ({
@@ -48,7 +49,19 @@ export default {
       email: '',
       password: '',
       remember: false
+    },
+  }),
+  methods: {
+    login () {
+      this.$store.dispatch('auth/login', {
+        email: this.form.email,
+        password: this.form.password
+      }).then( () => {
+        this.$router.replace({ name: 'welcome' })
+      }).catch( () => {
+        console.log('error')
+      })
     }
-  })
+  }
 }
 </script>
