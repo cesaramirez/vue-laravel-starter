3c7a4378241ef48423d4b36bc7e36038f205ebe4
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-01-06 10:37:59 -0600

    :bug: Fix Specified key was too long error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,21 +8,16 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
      * Register any application services.
-     *
-     * @return void
      */
     public function register()
     {
-        //
     }
 }
