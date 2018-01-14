cb936b853dd9bd564f3159ecd4d2ebbd1566f43c
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-01-14 08:45:08 -0600

    :package: update dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -34,26 +34,26 @@ mix.webpackConfig({
   resolve: { alias: { "@": path.join(__dirname, "./resources/assets/js") } }
 });
 
-if (mix.inProduction()) {
-  mix.webpackConfig({
-    plugins: [
-      new purgeCss({
-        paths: glob.sync([
-          path.join(__dirname, "resources/views/**/*.blade.php"),
-          path.join(__dirname, "resources/assets/js/**/*.vue")
-        ]),
-        extractors: [
-          {
-            extractor: class {
-              static extract(content) {
-                return content.match(/[A-z0-9-:\/]+/g);
-              }
-            },
-            extensions: ["html", "js", "php", "vue"]
-          }
-        ]
-      })
-    ],
-    resolve: { alias: { "@": path.join(__dirname, "./resources/assets/js") } }
-  });
-}
+// if (mix.inProduction()) {
+//   mix.webpackConfig({
+//     plugins: [
+//       new purgeCss({
+//         paths: glob.sync([
+//           path.join(__dirname, "resources/views/**/*.blade.php"),
+//           path.join(__dirname, "resources/assets/js/**/*.vue")
+//         ]),
+//         extractors: [
+//           {
+//             extractor: class {
+//               static extract(content) {
+//                 return content.match(/[A-z0-9-:\/]+/g);
+//               }
+//             },
+//             extensions: ["html", "js", "php", "vue"]
+//           }
+//         ]
+//       })
+//     ],
+//     resolve: { alias: { "@": path.join(__dirname, "./resources/assets/js") } }
+//   });
+// }
