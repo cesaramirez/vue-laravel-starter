776f0736f7714cf104ecf2c6812e6fff374f50a7
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-01-16 16:23:30 -0600

    :heavy_minus_sign: Remove PurgeCSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 let path = require("path");
 let mix = require("laravel-mix");
-let purgeCss = require("purgecss-webpack-plugin");
 let glob = require("glob-all");
 require("dotenv").config();
 
@@ -33,27 +32,3 @@ mix.webpackConfig({
   },
   resolve: { alias: { "@": path.join(__dirname, "./resources/assets/js") } }
 });
-
-// if (mix.inProduction()) {
-//   mix.webpackConfig({
-//     plugins: [
-//       new purgeCss({
-//         paths: glob.sync([
-//           path.join(__dirname, "resources/views/**/*.blade.php"),
-//           path.join(__dirname, "resources/assets/js/**/*.vue")
-//         ]),
-//         extractors: [
-//           {
-//             extractor: class {
-//               static extract(content) {
-//                 return content.match(/[A-z0-9-:\/]+/g);
-//               }
-//             },
-//             extensions: ["html", "js", "php", "vue"]
-//           }
-//         ]
-//       })
-//     ],
-//     resolve: { alias: { "@": path.join(__dirname, "./resources/assets/js") } }
-//   });
-// }
